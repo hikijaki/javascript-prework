06aeeffde74089680ac9af1982cf29fa33a54eaa
Michał Krokosz <michalino221@interia.pl>
2023-01-27 17:00:09 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -29,4 +29,33 @@ else if(playerInput == '2'){
 else {
     playerMove = 'nożyce';
 }
-printMessage('Twój ruch to: ' + playerMove);
+if( computerMove == 'kamień' && playerMove == 'papier'){
+  printMessage('Bot zagrał kamień a ty papier: Wygrywasz!');
+}
+else if (computerMove =='papier' && playerMove == 'kamień'){
+  printMessage('Bot zagrał papier a ty kamień: Przegrywasz!');
+}
+else if(computerMove =='nożyce' && playerMove == 'nożyce'){
+  printMessage('Bot zagrał nożyce a ty nożyce: Remis!');
+}
+else if(computerMove =='nożyce' && playerMove == 'papier'){
+  printMessage('Bot zagrał nożyce a ty papier: Przegrywasz!');
+}
+else if(computerMove =='papier' && playerMove == 'papier'){
+  printMessage('Bot zagrał papier a ty papier: Remis!');
+}
+else if(computerMove =='kamień' && playerMove == 'kamień'){
+  printMessage('Bot zagrał kamień a ty kamień: Remis!');
+}
+else if(computerMove =='kamień' && playerMove == 'nożyce'){
+  printMessage('Bot zagrał kamień a ty nożyce: Przegrywasz!');
+}
+else if(computerMove =='papier' && playerMove == 'nożyce'){
+  printMessage('Bot zagrał papier a ty nożyce: Wygrywasz!');
+}
+else if(computerMove =='nożyce' && playerMove == 'kamień'){
+  printMessage('Bot zagrał nożyce a ty kamień: Wygrywasz!!');
+}
+else {
+  printMessage('Nieznany ruch');
+}
